ab1414f9c2de14222d6db55a1c465aae33642778
qinqinluochao <349863137@qq.com>
2017-04-03 11:33:55 +0800

    添加gulp自动化环境

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angulargulpApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angulargulpApp
+ */
+angular.module('angulargulpApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
